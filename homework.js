273812904d86ed2a197d0704f7ebbd5885bf498f
dmadani <doha.madani@journalism.cuny.edu>
2016-02-15 11:29:33 -0500

    add files

---
+++ homework.js
@@ -0,0 +1,15 @@
+<script>
+
+var x = 5;
+var y = 8;
+var a = x + y;
+var m = x * y;
+var d = x / y;
+var s = x - y:
+
+document.getElementById("a").innerHTML = "When I add " + x + " to " + y + " I get " + a + ".";
+document.getElementById("s").innerHTML = "When I subtract " + x + " from " + y + " I get " + s + ".";
+document.getElementById("m").innerHTML = "When I multiply " + x + " and " + y + " I get " + m + ".";
+document.getElementById("d").innerHTML = "When I divide " + x + " by " + y + " I get " + d + ".";
+
+</script>
